6a7421cf6b9a5a784e9dad79528d533a29d9601a
vdriet <vdriet@gmail.com>
2016-03-24 14:09:58 +0000

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		String name = "world";
+		if(args.length > 0) {
+			name = args[0];
+		}
+		System.out.printf("Hello %s\n", name);
+	}
+}
